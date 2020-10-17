1ff20c8a1798c47acb257ad83cbc97e2cc47a88b
Viniboneto <vinicius.b.o.neto@gmail.com>
2020-10-17 00:13:14 -0300

    Fullstack Angular e Spring - 5.8. Desafio: Removendo lancamentos

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -13,12 +13,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoney.api.event.RecursoCriadoEvent;
@@ -71,6 +73,12 @@ public class LancamentoResource {
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancaSalvo);
 	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		lancaRep.deleteById(codigo);
+	}	
 	
 	/*
 	 * Não devemos tratar a exceção PessoaInexistenteOuInativaException no handler geral AlgamoneyExceptionHandler,
